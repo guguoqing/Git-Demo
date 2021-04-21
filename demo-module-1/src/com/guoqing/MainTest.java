cf6ff2a1eb9663b43fdadee1b7725d2ca40f4a72
guguoqing <17888803650@163.com>
2021-04-21 16:24:14 +0800

    分支冲突测试

--- demo-module-1/src/com/guoqing/MainTest.java
+++ demo-module-1/src/com/guoqing/MainTest.java
@@ -6,5 +6,7 @@ package com.guoqing;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello, git");
+
+        System.out.println("hello test");
     }
 }
